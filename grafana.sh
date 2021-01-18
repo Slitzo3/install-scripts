a0a0691e8a314974b0fefffab0368abd6111a878
Albin Håkanson <33166817+Tooxic@users.noreply.github.com>
2021-01-18 14:20:25 +0100

    Update grafana.sh

--- grafana.sh
+++ grafana.sh
@@ -2,10 +2,12 @@ sudo apt-get install -y apt-transport-https
 sudo apt-get install -y software-properties-common wget
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 
-echo "deb https://packages.grafana.com/enterprise/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 
-sudo apt-get update -y
-sudo apt-get install grafana-enterprise -y
+echo "deb https://packages.grafana.com/oss/deb beta main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+
+sudo apt-get update
+sudo apt-get install grafana
 
 cd /tmp
 
